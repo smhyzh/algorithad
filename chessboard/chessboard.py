7860aeea3a09ca0d2af9ff82e6df6d90c82d69f5
smhyzh <yangzhenghongdee@163.com>
2017-05-09 11:06:22 +0800

    增加L型骨牌类
    增加L型骨牌类，根据L型交点位置和L型骨牌所包含的外部块的位置构造出一个骨牌的三个块的位置。

--- chessboard/chessboard.py
+++ chessboard/chessboard.py
@@ -85,7 +85,56 @@ class RelativeChessBoard():
         return 'relative row:{} ,relative col:{} ; row:{} , col:{}'.format(
             self._rel_row,self._rel_col,self._row,self._col)
 
+class DominoL():
+    '''
+    L type domino.
+        corner_block:the pos(rwo,col) of corner block of L domino.
+        out_block:the pos(row,col) of block which next to L domino.
+    '''
+    def __init__(self,corner_block=(0,0),out_block=(0,0)):
+
+        self._blocks = self._getBlocksPos(corner_block,out_block)
+
+    def _getBlocksPos(self,corner_block,out_block):
+        '''
+        Get the three pos of block by corner block and out block.
+        '''
+        if corner_block[0] + 1 == out_block[0]:
+            # out block at the bottom of corner block.
+            if corner_block[1] + 1 == out_block[1]:
+                # out block at the right of corner block.
+                one_side = (corner_block[0]+1,corner_block[1])
+                other_side = (corner_block[0],corner_block[1]+1)
+                return [one_side,corner_block,other_side]
+            elif corner_block[1] - 1 == out_block[1]:
+                # out block at the left of corner block.
+                one_side = (corner_block[0]+1,corner_block[1])
+                other_side = (corner_block[0],corner_block[1]-1)
+                return [one_side,corner_block,other_side]
+            else:
+                raise ValueError('corner block and out block must be neighbor!')
+        elif corner_block[0] - 1 == out_block[0]:
+            # out block at the up of corner block.
+            if corner_block[1] + 1 == out_block[1]:
+                # out block at the right of corner block.
+                one_side = (corner_block[0]-1,corner_block[1])
+                other_side = (corner_block[0],corner_block[1]+1)
+                return [one_side,corner_block,other_side]
+            elif corner_block[1] - 1 == out_block[1]:
+                # out block at the left of corner block.
+                one_side = (corner_block[0]-1,corner_block[1])
+                other_side = (corner_block[0],corner_block[1]-1)
+                return [one_side,corner_block,other_side]
+            else:
+                raise ValueError('corner block and out block must be neighbor!')
+        else:
+            raise ValueError('corner block and out block must be neighbor!')
+
 class SpecialChessBoard():
+    '''
+    This class is for special chessboard.
+        chessboard:a instance of ChessBoard or RelativeChessBoard.
+    '''
     def __init__(self,chessboard):
         self._chessboard = chessboard
 
@@ -94,6 +143,19 @@ class SpecialChessBoard():
 
     def __str__(self):
         return str(self._chessboard)
+
+    def setSpecialBlock(self,spb_row,spb_col):
+        '''
+        Set special block for this special chessboard.
+            spb_row:the special block row.
+            spb_col:the special bloc col.
+        '''
+        self._spb_row = spb_row
+        self._spb_col = spb_col
+
+    def getFillPath(self):
+        pass
+
 
 def main():
     cb = ChessBoard(2,4)
